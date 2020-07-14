c59c05122910a25b1a425fd89ad18f16cc37f72d
Ravikesh Yadav <ravikesh@netzary.com>
2020-07-14 12:13:30 +0530

    Updates

---
+++ precache-manifest.7ace4300951b6bc025d0511ba3388d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae3ae8f81e5bbdfa8df276a9cd9ee47",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "1f71776a8f8cf8befbc3",
+    "url": "/netflix/static/css/main.d9b61d3b.chunk.css"
+  },
+  {
+    "revision": "348b01e14763e29d5f21",
+    "url": "/netflix/static/js/2.95208a87.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix/static/js/2.95208a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f71776a8f8cf8befbc3",
+    "url": "/netflix/static/js/main.099abb84.chunk.js"
+  },
+  {
+    "revision": "2dddb01aa0a4e9e8f0b8",
+    "url": "/netflix/static/js/runtime-main.5625af0b.js"
+  }
+]);
